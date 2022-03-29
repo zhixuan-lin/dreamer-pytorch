e803f079848583605251cd8c0b288c654902d371
zhixuan-lin <zxlin.cs@gmail.com>
2022-03-29 12:10:14 -0400

    Finish all modules except for RSSM

--- models.py
+++ models.py
@@ -1,9 +1,11 @@
 from gym import spaces
+import math
+import numpy as np
 import torch
 from torch import nn
 from torch.nn import functional as F
 from torch import Tensor
-from torch.distributions import Normal
+from torch.distributions import Normal, Independent, Bernoulli, TransformedDistribution, TanhTransform, Categorical
 from typing import Optional, Dict, Tuple
 
 
@@ -126,50 +128,158 @@ class ParallelApply(nn.Module):
 
 class ConvEncoder(nn.Module):
     def __init__(self, depth: int = 32, act: nn.Module = nn.ReLU):
+        super().__init__()
         self.depth = depth
         self.conv1 = nn.Conv2d(3, 1 * depth, kernel_size=4, stride=2)
         self.conv2 = nn.Conv2d(1 * depth, 2 * depth, kernel_size=4, stride=2)
         self.conv3 = nn.Conv2d(2 * depth, 4 * depth, kernel_size=4, stride=2)
         self.conv4 = nn.Conv2d(4 * depth, 8 * depth, kernel_size=4, stride=2)
+        # 64 -> 31 -> 14 -> 6 -> 2
         self.act = act()
 
-    @parallel_apply
     def forward(self, obs: Dict[str, Tensor]):
         x = obs['image']
+
+        T, B, *OTHER = x.size()
+        x = x.view(T * B, *OTHER)
+
         x = self.act(self.conv1(x))
         x = self.act(self.conv2(x))
         x = self.act(self.conv3(x))
         x = self.act(self.conv4(x))
         x = x.flatten(start_dim=-3)
+
+        TB, *OTHER = x.size()
+        x = x.view(T, B, *OTHER)
+
         assert x.size(-1) == 32 * self.depth
         return x
 
 
-class ConvDncoder(nn.Module):
+class ConvDecoder(nn.Module):
     def __init__(self, feature_dim: int, depth: int = 32, act: nn.Module = nn.ReLU, shape: Tuple[int, int, int] = (3, 64, 64)):
+        super().__init__()
         self.depth = depth
         self.fc = nn.Linear(feature_dim, 32 * depth)
-        self.conv1 = nn.ConvTranspose2d(3, 4 * depth, kernel_size=5, stride=2)
+        self.conv1 = nn.ConvTranspose2d(32 * depth, 4 * depth, kernel_size=5, stride=2)
         self.conv2 = nn.ConvTranspose2d(4 * depth, 2 * depth, kernel_size=5, stride=2)
         self.conv3 = nn.ConvTranspose2d(2 * depth, 1 * depth, kernel_size=6, stride=2)
         self.conv4 = nn.ConvTranspose2d(1 * depth, 3, kernel_size=6, stride=2)
+        self.shape = shape
         self.act = act()
 
-    @parallel_apply
     def forward(self, features: torch.Tensor):
+
         x = self.fc(features)
+
+        T, B, *OTHER = x.size()
+        x = x.view(T * B, *OTHER)
+
         x = x[:, :, None, None]  # (B, C, 1, 1)
         x = self.act(self.conv1(x))
         x = self.act(self.conv2(x))
         x = self.act(self.conv3(x))
         mean = self.conv4(x)
-        return Normal(mean, 1.0)
+        assert mean.size()[-3:] == self.shape
+        # Last 3 dims
+        TB, *OTHER = mean.size()
+        mean = mean.view(T, B, *OTHER)
+        return Independent(Normal(mean, 1.0), reinterpreted_batch_ndims=len(self.shape))
 
 
 
 class DenseDecoder(nn.Module):
-    pass
+    def __init__(self, input_dim: int, shape: Tuple[int, ...], layers: int, units: int, dist: str = 'normal', act: nn.Module = nn.ELU):
+        super().__init__()
+        self.shape = shape
+        self.layers = layers
+        self.units = units
+        self.dist = dist
+        self.act = act()
+        self.fc_layers = nn.ModuleList()
+        for i in range(layers):
+            self.fc_layers.append(nn.Linear(input_dim, units))
+            input_dim = units
+        self.fc_output = nn.Linear(units, np.prod(self.shape))
+
+
+    def forward(self, features: Tensor):
+        x = features
+        for layer in self.fc_layers:
+            x = layer(x)
+            x = self.act(x)
+        x = self.fc_output(x)
+        x = x.reshape(*x.size()[:-1], *self.shape)
+        if self.dist == 'normal':
+            return Independent(Normal(x, 1.0), reinterpreted_batch_ndims=len(self.shape))
+        elif self.dist == 'binary':
+            return Independent(Bernoulli(logits=x), reinterpreted_batch_ndims=len(self.shape))
+        else:
+            raise ValueError()
 
 
 class ActionDecoder(nn.Module):
-    pass
+    def __init__(
+          self, input_dim, size, layers, units, dist='tanh_normal', act=nn.ELU,
+          min_std=1e-4, init_std=5, mean_scale=5):
+        super().__init__()
+        self.size = size
+        self.layers = layers
+        self.units = units
+        self.dist = dist
+        self.act = act()
+        self.min_std = min_std
+        self.init_std = init_std
+        self.mean_scale = mean_scale
+        self.fc_layers = nn.ModuleList()
+        for i in range(layers):
+            self.fc_layers.append(nn.Linear(input_dim, units))
+            input_dim = units
+        self.fc_output = nn.Linear(units, size * 2 if self.dist == 'tanh_normal' else size)
+        self.raw_init_std = math.log(math.exp(init_std - 1))
+
+    def forward(self, features: Tensor):
+        x = features
+        for layer in self.fc_layers:
+            x = layer(x)
+            x = self.act(x)
+        x = self.fc_output(x)
+        if self.dist == 'tanh_normal':
+            mean, std = x.chunk(2, dim=-1)
+            mean = self.mean_scale * torch.tanh(mean / self.mean_scale)
+            std = F.softplus(std + self.raw_init_std) + self.min_std
+            dist = Normal(mean, std)
+            dist = TransformedDistribution(dist, TanhTransform())
+            dist = Independent(dist, 1)
+        elif self.dist == 'onehot':
+            dist = Categorical(logits=x)
+        else:
+            raise ValueError()
+        return dist
+
+
+if __name__ == '__main__':
+    def test_conv():
+        encoder = ConvEncoder()
+        decoder = ConvDecoder(feature_dim=32 * 8 * 4)
+        input = torch.randn(4, 3, 3, 64, 64)
+        feature = encoder(dict(image=input))
+        output = decoder(feature)
+
+        assert input.size() == output.base_dist.mean.size()
+
+    def test_dense():
+        dense_decoder = DenseDecoder(input_dim=64, shape=(7, 13), layers=3, units=128)
+        input = torch.randn(4, 64)
+        print(dense_decoder(input))
+
+    def test_action():
+        action_decoder = ActionDecoder(input_dim=64, size=13, layers=3, units=128)
+        input = torch.randn(4, 64)
+        print(action_decoder(input).sample().size())
+
+
+
+    # test_conv()
+    # test_dense()
+    test_action()
